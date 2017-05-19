91273244229a86bbf9954417997d1287142a3f30
Gabriel Silva Tosetti <gabriel.tosetti@hotmail.com>
2017-05-19 17:35:23 -0300

    commit de todos os arquivos

---
+++ resources/views/layouts/principal.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notify - @yield('title') </title>
+
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+
+  <!-- Wrapper-->
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        @include('layouts.navigation')
+
+        <!-- Page wraper -->
+        <div id="page-wrapper" class="gray-bg">
+
+            <!-- Page wrapper -->
+            @include('layouts.topnavbar')
+
+            <!-- Main view  -->
+            @yield('content')
+
+            <!-- Footer -->
+            @include('layouts.footer')
+
+        </div>
+        <!-- End page wrapper-->
+
+    </div>
+    <!-- End wrapper-->
+
+<script src="{!! asset('js/app.js') !!}" type="text/javascript"></script>
+
+@section('scripts')
+@show
+
+</body>
+</html>
