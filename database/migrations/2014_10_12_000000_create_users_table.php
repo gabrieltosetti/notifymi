06afca3cf77675a662349d904880fce418dc84a8
matheusluz96 <matheusluz96@gmail.com>
2017-06-28 18:46:11 -0300

    Adicionando permissão as migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('avatar')->default('default.jpg');
+            $table->string('permissao');
             $table->rememberToken();
             $table->timestamps();
         });
