ef46d37605b933ab588f8f9861c05897f10405ba
Rafaelblud <rafael-bella@hotmail.com>
2017-07-10 22:13:58 -0300

    padronizando rotas

--- routes/web.php
+++ routes/web.php
@@ -21,39 +21,27 @@ Route::get('/', 'HomeController@index2')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/2', 'HomeController@index2')->name('home2');
 
-//Consertos
-Route::get('/consertos', 'ConsertoController@lista')->name('consertos');
-Route::get('/consertos/novo', 'ConsertoController@novo')->name('novo_conserto');
-
 //login
-Route::get('/entrar/assistencia', 'LoginController@loginassistencia')->name('entrarassistencia');
-Route::get('/entrar/cliente', 'LoginController@logincliente')->name('entrarcliente');
-Route::get('/entrar/cadastro', 'LoginController@cadastro')->name('cadastro');
+Route::get('/assistencia/login', 'LoginController@loginassistencia')->name('entrarassistencia');
+Route::get('/cadastro', 'LoginController@cadastro')->name('cadastro');
 
-  //admin Login
-  Route::get('/adminindex', 'AdminController@index')->name('admin');
-  Route::get('/entrar/admin', 'Auth\AdminLoginController@showLoginForm')->name('entraradmin');
-  Route::post('/entrar/admin', 'Auth\AdminLoginController@Login')->name('entraradmin.submit');
-
+//ADMIN
+Route::get('/admin/index', 'AdminController@index')->name('admin');
+Route::get('/admin/entrar', 'Auth\AdminLoginController@showLoginForm')->name('entraradmin');
+Route::post('/admin/entrar', 'Auth\AdminLoginController@Login')->name('entraradmin.submit');
 
 //profile
-Route::get('/profile', 'ProfileController@perfil')->name('perfil');
-Route::post('/profile', 'ProfileController@mudaravatar')->name('perfil_post');
+Route::get('/perfil', 'ProfileController@perfil')->name('perfil');
+Route::post('/perfil', 'ProfileController@mudaravatar')->name('perfil_post');
 
 //Cliente
+Route::get('/clientes/login', 'LoginController@logincliente')->name('entrarcliente');
 Route::get('/clientes', 'ClienteController@lista')->name('clientes');
 Route::get('/clientes/novo', 'ClienteController@novo')->name('novo_cliente');
-
-
-
 Route::get('/clientes/detalhes/{id}/', 'ClienteController@detalhes')->name('detalhes_cliente');
 Route::post('/clientes/adiciona', 'ClienteController@adiciona')->name('novo_cliente_post');
-Route::get('/clientes/remove/{id}', 'ClienteController@remove')->name('remove_cliente');
+Route::delete('/clientes/remove/{id?}', 'ClienteController@remove')->name('remove_cliente');
 Route::get('/clientes/edita/{id}', 'ClienteController@edita')->name('edita_cliente');
-
-//usuarios
-Route::get('/usuarios', 'UsuarioController@lista')->name('usuarios');
-Route::get('/usuarios/novo', 'UsuarioController@novo')->name('novo_usuario');
 
 //permissoes
 Route::get('/permissoes', 'PermissoesController@lista')->name('permissoes');
@@ -63,20 +51,18 @@ Route::get('/permissoes/novo', 'PermissoesController@novo')->name('novo_permisso
 Route::get('/cargos', 'CargosController@listacargos')->name('cargos');
 Route::get('/cargos/novo', 'CargosController@novocargo')->name('novo_cargos');
 
-
+//usuarios
+Route::get('/usuarios', 'UsuarioController@lista')->name('usuarios');
+Route::get('/usuarios/novo', 'UsuarioController@novo')->name('novo_usuario');
 Route::get('/usuarios/detalhes/{id?}', 'UsuarioController@detalhes')->name('detalhes_usuario');
 Route::post('/usuarios/adiciona', 'UsuarioController@adiciona')->name('novo_usuario_post');
 Route::delete('/usuarios/remove/{id?}', 'UsuarioController@remove')->name('remove_usuario');
 Route::get('/usuarios/edita/{id}', 'UsuarioController@edita')->name('edita_usuario');
 
-Route::get('/clientes/detalhes/{id?}', 'ClienteController@detalhes')->name('detalhes_cliente');
-Route::post('/clientes/adiciona', 'ClienteController@adiciona')->name('novo_cliente_post');
-Route::delete('/clientes/remove/{id?}', 'ClienteController@remove')->name('remove_cliente');
-Route::get('/clientes/edita/{id}', 'ClienteController@edita')->name('edita_cliente');
-
+//Consertos
+Route::get('/consertos', 'ConsertoController@lista')->name('consertos');
+Route::get('/consertos/novo', 'ConsertoController@novo')->name('novo_conserto');
 Route::get('/consertos/detalhes/{id?}', 'ConsertoController@detalhes')->name('detalhes_conserto');
 Route::post('/consertos/adiciona', 'ConsertoController@adiciona')->name('novo_conserto_post');
 Route::get('/consertos/remove/{id}', 'ConsertoController@remove')->name('remove_conserto');
 Route::get('/consertos/edita/{id}', 'ConsertoController@edita')->name('edita_conserto');
-
-Auth::routes();
