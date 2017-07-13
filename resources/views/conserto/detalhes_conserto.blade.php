853c19fe2170a3cc044c6ecc575e96efde24b062
Gabriel Silva Tosetti <gabriel.tosetti@hotmail.com>
2017-07-13 01:01:52 -0300

    Conserto - Detalhamento v2

--- resources/views/conserto/detalhes_conserto.blade.php
+++ resources/views/conserto/detalhes_conserto.blade.php
@@ -21,13 +21,63 @@
                 <div class="ibox-title">
                     <h5></h5>
                     <div class="ibox-tools">
-                        <a href="" class="btn btn-primary btn-xs">Novo conserto</a>
+                        <a href="" class="btn btn-primary btn-xs" >Novo conserto</a>
                     </div>
                 </div><!-- /IBOX TITLE -->
                 <div class="ibox-content">
                     <div class="row">
-                        <div class="col-sm-4 border-right">
-                            <img alt="usuário" class="img-thumbnail img-lg img-responsive" src="/media/avatars/default.jpg">
+                        <div class="col-md-4 border-right border-size-sm">
+                            <!-- IMAGEM -->
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <img alt="usuário" class="img-thumbnail img-md pull-left m-r-xs" src="/media/avatars/default.jpg">
+                                    <strong>Criado por </strong><a href="#">Gabriel Tosetti</a> <br>
+                                    13/07/2017 00:07
+                                </div>
+                            </div>
+                            <!-- /IMAGEM -->
+                            <div class="row m-t-md form-horizontal border-bottom">
+                                <!-- CLIENTE -->
+                                <label class="control-label col-md-3">Cliente: </label>
+                                <p class="form-control-static col-md-9"> Matheus Luz<a href="#" class="pull-right" data-toggle="tooltip" data-placement="top" title="Mudar cliente"><i class="fa fa-pencil m-r-sm"></i></a></p>
+                                <!-- /CLIENTE -->
+                                <!-- CRIADO -->
+                                <label class="control-label col-md-3">Criado: </label>
+                                <p class="form-control-static col-md-9"> 13/07/2017 00:07</p>
+                                <!-- /CRIADO -->
+                                <!-- ATUALIZADO -->
+                                <label class="control-label col-md-3">Atualizado: </label>
+                                <p class="form-control-static col-md-9"> 13/07/2017 00:41</p>
+                                <!-- /ATUALIZADO -->
+                            </div>
+                            <div class="row m-t-md form-horizontal">
+                                <!-- PRIORIDADE -->
+                                <div class="row">
+                                    <label class="control-label col-md-3">Prioridade: </label>
+                                    <div class="col-md-5">
+                                        <select class="form-control" name="prioridade">
+                                            <option value="1">Baixo</option>
+                                            <option value="2">Médio</option>
+                                            <option value="3">Alto</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <!-- /PRIORIDADE -->
+                                <!-- STATUS -->
+                                <div class="row m-t-sm">
+                                    <label class="control-label col-md-3">Status: </label>
+                                    <div class="col-md-5">
+                                        <select class="form-control" name="status">
+                                            <option value="aberto">Aberto</option>
+                                            <option value="andamento">Em andamento</option>
+                                            <option value="pendente">Pendente</option>
+                                            <option value="fechado">Fechado</option>
+                                            <option value="cancelado">Cancelado</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <!-- /STATUS -->
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -40,6 +90,9 @@
 @section('scripts')
 <script>
 
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
 
 </script>
 
