1bf1b3cd5f17cbb63912bd1ebf4d2dcf64021a00
matheusluz96 <matheusluz96@gmail.com>
2017-07-19 20:13:18 -0300

    removendo algumas imagens e adicionando icone

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notify - @yield('title') </title>
-
+    <link rel="icon" href="{!! asset('img/logob.ico') !!}"/>
 
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
