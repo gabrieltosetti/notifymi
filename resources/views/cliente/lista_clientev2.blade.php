1f953e936cfd5be4efe178b817ea83c03c784e19
matheusluz96 <matheusluz96@gmail.com>
2017-07-24 19:08:32 -0300

    Adicionando detalhes clientes

--- resources/views/cliente/lista_clientev2.blade.php
+++ resources/views/cliente/lista_clientev2.blade.php
@@ -17,7 +17,7 @@
 
 @section('content')
 <div class="wrapper wrapper-content animated fadeInRight">
-  <table  class="table table-striped table-bordered table-hover issue-tracker dataTables-clientes" id="lista_cliente" cellspacing="0">
+  <table  class="table table-striped table-bordered table-hover issue-tracker dataTables-clientes" cellspacing="0">
     <thead>
       <tr>
         <th>ID</th>
@@ -31,10 +31,12 @@
       </tr>
     </thead>
     <tbody>
-      @foreach($clientes as $cliente)
-      <tr id="cliente-{{$cliente->id}}">
+      <div class="row">
 
-        <td class="cliente-id"> <!-- ID -->
+      @foreach($clientes as $cliente)
+      <tr id="lista_cliente">
+
+        <td class="cliente-id" id="cliente-nome-{{$cliente->id}}"> <!-- ID -->
           #{{$cliente->id}}
         </td>
 
@@ -43,7 +45,7 @@
         </td>
 
         <td>    <!-- AVATAR -->
-          <h3><strong id="cliente-nome-{{$cliente->id}}">{{$cliente->nome}}</strong></h3>      <!-- NOME -->
+          <h3><strong >{{$cliente->nome}}</strong></h3>      <!-- NOME -->
         </td>
         <td>
           {{$cliente->email}}
@@ -92,17 +94,129 @@
         <div class="col-xs-4 col-sm-4 hidden-xs">
 
         </div>
-
-
-        <!--CELULAR-->
-
-
       </div>
     </div>
-  </div> <!--/col lg 4-->
-  @endforeach
+  </div>
   <!--MODAL-->
 
+
+
+
+
+  <div class="modal inmodal" id="modal-detalhes" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+          <div class="modal-content animated bounceInRight">
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                  <img alt="image" class="img-circle img-responsive img-lg center-block" src="/media/avatars/{{$cliente->avatar}}">
+                  <h4 class="modal-title" id="modal-titulo"></h4>
+                  <small class="font-bold" ></small>
+              </div>
+              <div class="modal-body">
+                  <div class="form-horizontal">
+                      <div class="row">
+                          <div class="col-xs-6">
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">RG</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-RG"></p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-xs-6">
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">CPF</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-CPF"></p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+      <!--divisoria--><div class="hr-line-dashed"></div> <!--divisoria-->
+                      <div class="row">
+                          <div class="col-xs-6">
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">E-mail</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-e-mail"></p>
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">Celular</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-celular"></p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-xs-6">
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">Telefone</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-telefone"></p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+      <!--divisoria--><div class="hr-line-dashed"></div> <!--divisoria-->
+                      <div class="row">
+                          <div class="col-xs-6">
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">Cidade</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-cidade"></p>
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">Bairro</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-bairro"></p>
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">Rua</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-rua"></p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-xs-6">
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">Número</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-numero"></p>
+                                  </div>
+                              </div>
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">Comple.</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-complemento"></p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+      <!--divisoria--><div class="hr-line-dashed"></div> <!--divisoria-->
+                      <div class="row">
+                          <div class="col-xs-6">
+                              <div class="form-group">
+                                  <label class="col-sm-3 col-xs-12 control-label">Permissão</label>
+                                  <div class="col-sm-9">
+                                      <p class="form-control-static" id="modal-permissao"></p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+          </div><!--/modal content-->
+      </div>
+  </div>
+  <!--/MODAL-->
+  @endforeach
+</div>
 </tbody>
 <tfoot><tr></tr></tfoot>
 </table>
