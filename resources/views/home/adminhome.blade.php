d2f0f8356a379147c05f27604b0ba7aff84241e6
Rafaelblud <rafael-bella@hotmail.com>
2017-08-14 19:42:52 -0300

    teste
    admin home no PC não ta mudando poura

--- resources/views/home/adminhome.blade.php
+++ resources/views/home/adminhome.blade.php
@@ -1,132 +1,54 @@
 @extends('layouts.principal', ['breadcrumb' => 'false'])
-
 @section('title', 'Página Inicial')
 <!--ACHO QUE A HOME NAO DEVERIA TER OS BREADCRUMBS E SER PERSONALIZADA COMO UMA DASHBOARD...-->
 @section('content')
 
-        <!--
-            WIDGETS
-        -->
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="widget style1 navy-bg">
-                    <div class="row">
-                        <div class="col-xs-4">
-                            <i class="fa fa-cloud fa-5x"></i>
-                        </div>
-                        <div class="col-xs-8 text-right">
-                            <span> Today degrees </span>
-                            <h2 class="font-bold">26'C</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="widget style1 lazur-bg">
-                    <div class="row">
-                        <div class="col-xs-4">
-                            <i class="fa fa-envelope-o fa-5x"></i>
-                        </div>
-                        <div class="col-xs-8 text-right">
-                            <span>  </span>
-                            <h2 class="font-bold">260</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="widget style1 yellow-bg">
-                    <div class="row">
-                        <div class="col-xs-4">
-                            <i class="fa fa-music fa-5x"></i>
-                        </div>
-                        <div class="col-xs-8 text-right">
-                            <span> New albums </span>
-                            <h2 class="font-bold">12</h2>
-                        </div>
-                    </div>
-                </div>
+<!--WIDGETS-->
+<div class="row">
+    <div class="col-lg-6">
+        <div class="ibox-content">
+            <h2>TODO List</h2>
+            <small>This is example of task list</small>
+            <ul class="todo-list m-t">
+                <li>
+                    <input type="checkbox" value="" name="" class="i-checks"/>
+                    <span class="m-l-xs">Buy a milk</span>
+                    <small class="label label-primary"><i class="fa fa-clock-o"></i> 1 mins</small>
+                </li>
+                <li>
+                    <input type="checkbox" value="" name="" class="i-checks" checked/>
+                    <span class="m-l-xs">Go to shop and find some products.</span>
+                    <small class="label label-info"><i class="fa fa-clock-o"></i> 3 mins</small>
+                </li>
+                <li>
+                    <input type="checkbox" value="" name="" class="i-checks" />
+                    <span class="m-l-xs">Send documents to Mike</span>
+                    <small class="label label-warning"><i class="fa fa-clock-o"></i> 2 mins</small>
+                </li>
+                <li>
+                    <input type="checkbox" value="" name="" class="i-checks"/>
+                    <span class="m-l-xs">Go to the doctor dr Smith</span>
+                    <small class="label label-danger"><i class="fa fa-clock-o"></i> 42 mins</small>
+                </li>
+            </ul>
+        </div>
+    </div>
+<!--/WIDGETS-->
+<!--/TABELA DE CONSERTOS-->
+
+<div class="wrapper wrapper-content animated fadeInRight">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="text-center m-t-lg">
+                <h1>
+                    Notify-MI!
+                </h1>
+                <small>
+                    Seu sistema de notificações simplificado.
+                </small>
             </div>
         </div>
-        <!--/WIDGETS-->
-        <!--
-            TABELA DE CONSERTOS !!
-        -->
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="ibox float-e-margins">
-                    <div class="ibox-content">
-                        <h2>Consertos</h2>
-                        <div>
-                            <table class="table">
-                                <tbody>
-                                  <tr>
-                                      <td>
-                                          <button type="button" style="height:35px; width:50px" class="btn m-r-sm">{{$novo}}</button>
-                                          Novos
-                                      </td>
-                                  </tr>
-                                <tr>
-                                    <td>
-                                        <button type="button" style="height:35px; width:50px" class="btn btn-warning m-r-sm">{{$emandamento}}</button>
-                                        Em andamento
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <button type="button" style="height:35px; width:50px" class="btn btn-success m-r-sm">{{$emespera}}</button>
-                                        Em espera
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>
-                                        <button type="button" style="height:35px; width:50px" class="btn btn-primary m-r-sm">{{$concluido}}</button>
-                                        Concluído
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <button type="button" style="height:35px; width:50px" class="btn btn-danger m-r-sm">{{$cancelado}}</button>
-                                        Cancelado
-                                    </td>
-                                </tr>
-
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="ibox float-e-margins">
-                    <div class="ibox-title">
-                        <h5>Bar Chart Example</h5>
-                    </div>
-                    <div class="ibox-content">
-                        <div>
-                            <canvas id="barChart" height="140"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--/TABELA DE CONSERTOS-->
-
-            <div class="wrapper wrapper-content animated fadeInRight">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center m-t-lg">
-                            <h1>
-                                Notify-MI!
-                            </h1>
-                            <small>
-                                Seu sistema de notificações simplificado.
-                            </small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    <script src="js/plugins/chartJs/Chart.min.js"></script>
+    </div>
+</div>
+<script src="js/plugins/chartJs/Chart.min.js"></script>
 @endsection
